a7e1c3a5eb6015204e01585b907162cb5c46d1f1
Derik Suria <dsuria23@students.cmc.edu>
2023-02-06 14:07:57 -0800

    linter

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -4,8 +4,8 @@
 def validate_html(html):
 
     '''
-    This function performs a limited version of html 
-    validation by checking whether every opening tag 
+    This function performs a limited version of html
+    validation by checking whether every opening tag
     has a corresponding closing tag.
 
     >>> validate_html('<strong>example</strong>')
@@ -13,7 +13,7 @@ def validate_html(html):
     >>> validate_html('<strong>example')
     False
     '''
-    
+ 
     stack = []
     tags = _extract_tags(html)
     if len(tags) <= 1 and len(html) > 0:
@@ -36,13 +36,13 @@ def validate_html(html):
     # then process these html tags using the balanced parentheses algorithm from the class/book
     # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
     # but arbitrary text located between the html tags
-    
+
 
 def _extract_tags(html):
 
     '''
     This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not 
+    By convention in Python, helper functions that are not
     meant to be used directly by the user are prefixed with an underscore.
 
     This function returns a list of all the html tags contained in the input string,
